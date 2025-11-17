14376b3e3585862c733f47bfa188e2d7e1234bf4
Victoria Vrublevskaya <vivrubl16@gmail.com>
2025-11-17 18:29:39 +0400

    feat: implement isSameLength function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -149,8 +149,13 @@ function getAverage(/* arr */) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  if (arr.length < 2) {
+    return true;
+  }
+  
+  const firstLength = arr[0].length;
+  return arr.every((str) => str.length === firstLength);
 }
 
 /**
